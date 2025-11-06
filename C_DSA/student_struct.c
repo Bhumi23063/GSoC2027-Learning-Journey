67fb9e5bc3aad58a9c42aaa44d7376982d7aa91e
Bhumika G <bhumigowda9353@gmail.com>
2025-11-06 14:52:13 +0000

    Add topper dynamic struct program

---
+++ C_DSA/student_struct.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+struct student{
+    char name[100];
+    int roll_no;
+    float marks;
+};
+
+void main()
+{
+    int i , n;
+    struct student s[100];
+
+    printf("Enter the number of studnets : ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        printf("Enter the name , roll number and the marks of the student %d : ", i+1);
+        scanf("%s %d %f" ,&s[i].name,&s[i].roll_no,&s[i].marks); 
+    }
+
+    for(i=0;i<n;i++)
+    {
+        printf("The details of the student %d are : \nName : %s \nRoll Number : %d \nMarks : %f\n", i+1, s[i].name, s[i].roll_no, s[i].marks);
+    }
+
+    return 0;
+}
+
